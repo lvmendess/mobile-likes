51ede714e205710f53075ac9952b527b715b2702
lvmendess <liviamendes1703@gmail.com>
2025-07-27 18:33:03 -0300

    readme

--- ex1/app/src/main/java/com/example/ex1/MainActivity.java
+++ ex1/app/src/main/java/com/example/ex1/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
